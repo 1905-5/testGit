0958c9b6408cef072915ceb02425c1b96d35b604
WangLin <472610018@qq.com>
2019-11-28 15:54:59 +0800

    清除实体1

--- test-git/src/main/java/com/jk/controller/bookController.java
+++ test-git/src/main/java/com/jk/controller/bookController.java
@@ -5,25 +5,5 @@ public class bookController {
     private Integer bookid;
     private String bookName;
 
-    private Integer bookPrice;
-    private String bookDate;
-
-    private String bookDate1;
-    private String bookDate2;
-
-    private String bookDate3;
-    private String bookDate4;
-
-    private String bookDate5;
-    private String bookDate6;
-    private String bookDate7;
-    private String bookDate8;
-
-    private String bookDate9;
-    private String bookDate10;
-    private String bookDate11;
-    private String bookDate12;
-
-
 
 }
